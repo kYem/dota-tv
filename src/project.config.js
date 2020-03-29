3c0ed6f893315c0784e5ab5925477737376844d7
Kestutis Kasiulynas <k.kasiulynas@gmail.com>
2020-03-29 00:25:05 +0000

    Add env-cmd package
    Setup prod env build

--- src/project.config.js
+++ src/project.config.js
@@ -1,6 +1,6 @@
 export default {
-  apiHostname: 'http://127.0.0.1:8008',
+  apiHostname: process.env.REACT_APP_API_HOSTNAME || 'http://127.0.0.1:8008',
   dotaImageCdn: 'https://api.opendota.com/apps/dota2/images',
-  wsEndpoint: 'ws://127.0.0.1:8008/ws',
+  wsEndpoint: process.env.REACT_APP_WS_ENDPOINT || 'ws://127.0.0.1:8008/ws',
   isProd: false,
 }
