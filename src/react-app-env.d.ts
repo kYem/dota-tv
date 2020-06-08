aabe7124459dc82eb801aee39c6b47d1972ebb0d
Kestutis Kasiulynas <k.kasiulynas@gmail.com>
2020-06-09 00:54:52 +0100

    Conver twitch player to tsx.
    Add global window.Twitch interface

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="react-scripts" />
+
+
+interface TwitchPlayerEmbed extends HTMLDivElement {
+  getPlayer: () => any
+}
+
+interface EmbedTwitchPlayer extends HTMLDivElement {
+  new (targetClass: string, props: TwitchPlayerBaseProps): TwitchPlayerEmbed;
+  AUTHENTICATE: string;
+  VIDEO_PLAY: string;
+  VIDEO_READY: string;
+}
+
+interface Window {
+  Twitch: {
+    Embed: EmbedTwitchPlayer
+  };
+}
