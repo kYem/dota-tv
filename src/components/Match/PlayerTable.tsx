524771ff0a5363720eba04f4b6b48597ef180f53
Kestutis Kasiulynas <k.kasiulynas@gmail.com>
2021-07-13 22:54:32 +0100

    Table should be responsive in mobile view
    Add scrollbar atm.

--- src/components/Match/PlayerTable.tsx
+++ src/components/Match/PlayerTable.tsx
@@ -3,10 +3,11 @@ import './PlayerTable.scss'
 import PlayerRow from './PlayerRow'
 import { Player } from '../../models/LiveMatchData'
 
-const PlayerTable = ({ players }: { players: Player[]}) => (
+const PlayerTable = ({ players }: { players: Player[] }) => (
 
-  <table className='table player-match-table table-striped table-hover'>
-    <thead className='table-dark'>
+  <div className={'table-responsive mb-3'}>
+    <table className="table player-match-table table-striped table-hover mb-0">
+      <thead className="table-dark">
       <tr>
         <th>Player</th>
         <th>Level</th>
@@ -14,14 +15,15 @@ const PlayerTable = ({ players }: { players: Player[]}) => (
         <th>LH/DN</th>
         <th>Gold</th>
       </tr>
-    </thead>
-    <tbody>
+      </thead>
+      <tbody>
       <tr />
-    </tbody>
-    <tbody>
+      </tbody>
+      <tbody>
       {players.map(player => (<PlayerRow key={player.account_id} {...player} />))}
-    </tbody>
-  </table>
+      </tbody>
+    </table>
+  </div>
 )
 
 export default PlayerTable
