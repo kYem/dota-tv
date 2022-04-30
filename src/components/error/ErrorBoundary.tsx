991b485517651fe6cab49bf787af40a20977d47a
Kestutis Kasiulynas <k.kasiulynas@gmail.com>
2022-04-30 15:03:35 +0100

    Fix children warning for ErrorBoundary

--- src/components/error/ErrorBoundary.tsx
+++ src/components/error/ErrorBoundary.tsx
@@ -4,13 +4,13 @@ function logErrorToMyService(error: Error, errorInfo: React.ErrorInfo) {
   console.log(error, errorInfo);
 }
 
-export class ErrorBoundary extends React.Component<unknown, { hasError: boolean }> {
-  constructor(props: unknown) {
+export class ErrorBoundary extends React.Component<{ children?: JSX.Element }, { hasError: boolean }> {
+  constructor(props: Readonly<{ children?: JSX.Element }>) {
     super(props);
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error: Error) {
+  static getDerivedStateFromError() {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
